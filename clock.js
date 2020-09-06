c347f7e7d579c2eec4c407d767f82a1f7fddaca5
ikasoba <ikasoba000@users.noreply.github.com>
2020-09-06 17:09:11 +0900

    スマホ用cssも変更

--- clock.js
+++
@@ -1,8 +0,0 @@
-function clock() { 
-var date = new Date();
-var h = date.getHours();
-var m  = date.getMinutes();
-var aaa = "" + h + ":" + m + "";
-document.getElementById("clock").innerHTML = aaa;
-}
-setInterval('clock()',1000);
